494f818025719e9c1127fe79fc4d33e0a5bdbc94
andre.redenius <andre.redenius@webid-solutions.de>
2026-05-20 17:03:14 +0200

    Bump app version to 1.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -82,8 +82,8 @@ android {
         applicationId = "de.zugspitz.supporter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     signingConfigs {
         create("release") {
