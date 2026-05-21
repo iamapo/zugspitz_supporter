37e82b4c53daeecadb407f49c80c5a09ae9f0e27
iamapo <redenius.andre@googlemail.com>
2026-05-21 08:41:14 +0200

    fixed crash

--- composeApp/src/iosMain/kotlin/de/zugspitz/supporter/LiveSharingLogger.ios.kt
+++ composeApp/src/iosMain/kotlin/de/zugspitz/supporter/LiveSharingLogger.ios.kt
@@ -4,11 +4,11 @@ import platform.Foundation.NSLog
 
 internal actual object LiveSharingLogger {
     actual fun d(message: String) {
-        NSLog("LiveSharing: %@", message)
+        NSLog("LiveSharing: $message")
     }
 
     actual fun e(message: String, throwable: Throwable?) {
         val suffix = throwable?.let { " | ${it.message ?: it::class.simpleName}" }.orEmpty()
-        NSLog("LiveSharing ERROR: %@%@", message, suffix)
+        NSLog("LiveSharing ERROR: $message$suffix")
     }
 }
